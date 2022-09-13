f57aeb3455395294d1623d35eae112f8dfb30cce
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-09-13 23:52:51 +0300

    ready user-profile without styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import UserProfile from 'components/UserProfile';
+import user from './User.json';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,13 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+   <UserProfile 
+   name={user.username}
+   tag={user.tag}
+   location={user.location}
+   avatar={user.avatar}
+   stats={user.stats}/>
+
     </div>
   );
 };
