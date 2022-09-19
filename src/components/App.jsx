89092a028eeba07114eaaa3bc9b111736ab8ea49
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-09-19 10:43:45 +0300

    user ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,16 @@
-import UserProfile from 'components/UserProfile';
+import { UserProfile } from 'components/UserProfile/UserProfile';
 import user from 'components/User.json';
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
     >
-   <UserProfile 
-   name={user.username}
-   tag={user.tag}
-   location={user.location}
-   avatar={user.avatar}
-   stats={user.stats}/>
+      <UserProfile 
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}/>
 
     </div>
   );
