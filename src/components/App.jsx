c9ea4f88456d5fc3402534883d2ce2dade17f6d5
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-09-19 12:30:34 +0300

    statistics ready min css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import { UserProfile } from 'components/UserProfile/UserProfile';
+import { Statistics } from 'components/Statistic/Statistics';
 import user from 'components/User.json';
+import data from 'components/data.json';
 
 export const App = () => {
   return (
-    <div
-    >
+    <div>
+      
       <UserProfile 
       name={user.name}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}/>
+      
+      <Statistics
+      title="Upload stats"
+      stats={data} />
 
     </div>
   );
